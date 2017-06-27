9aac81d2faa28454c22f19bdf8104bfd330fe2db
Paulson Ps <paulsonps@neoito.com>
2017-06-28 01:23:24 +0530

    Panellist component page intagreted

---
+++ src/app/home/panellist/panellist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the toolbar component.
+ */
+@Component({
+  selector: 'pwa-panellist',
+  templateUrl: 'panellist.component.html'
+})
+export class PanellistComponent { }
+
