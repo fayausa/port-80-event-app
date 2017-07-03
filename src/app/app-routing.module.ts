1ac5fc12ef9a0d37a174b95d1e444b50792840ed
amalshehu <amalshehu@gmail.com>
2017-07-03 18:47:10 +0530

    Code cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'home', component: HomeComponent },
+  // { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'notify', component: MsgComponent },
   // { path: 'contact', component: ContactComponent }
