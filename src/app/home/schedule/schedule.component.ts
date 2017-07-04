e3676500c45f811220f22e7a95e8339660ba9fbb
Laiju Thomas <laiju@fayausa.com>
2017-07-04 12:10:22 +0530

    updated schedule

--- src/app/home/schedule/schedule.component.ts
+++ src/app/home/schedule/schedule.component.ts
@@ -7,6 +7,7 @@ import { Modal } from 'angular2-modal/plugins/bootstrap';
  */
 @Component({
   selector: 'pwa-schedule',
+  styleUrls: ['./schedule.scss'],
   templateUrl: 'schedule.component.html'
 })
 export class ScheduleComponent {
@@ -14,7 +15,66 @@ export class ScheduleComponent {
   isClicked: any;
   srcUrl = 'https://www.youtube.com/embed/LMlbRsiYgpw';
   disabled = false;
-  constructor(overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal) {}
+  filterDetailsArray: any = [];
+  filters = [
+    {
+      "id": 1,
+      "title": "Keynote",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 2,
+      "title": "Blockchain",
+      "bg_color_class": "bg_yellow"
+    },
+    {
+      "id": 3,
+      "title": "FOSS",
+      "bg_color_class": "bg_whitesmoke"
+    },
+    {
+      "id": 4,
+      "title": "Data Science",
+      "bg_color_class": "grey"
+    },
+    {
+      "id": 5,
+      "title": "Machine Learning",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 6,
+      "title": "AI",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 7,
+      "title": "Tensor Flow",
+      "bg_color_class": "bg_yellow"
+    },
+    {
+      "id": 8,
+      "title": "UX",
+      "bg_color_class": "bg_primary"
+    }
+  ]
+  schedule = [
+    {
+      "id": 1,
+      "title": "Tech Disruption – Opportunities & Threats",
+      "speaker": "Mr. Michael Gord (Founder & CEO - MLG Blockchain Consulting)",
+      "time": "10:00 AM",
+      "stage": "Travancore Hall (Park Center)",
+      "filters": [1,2],
+      "video_url": "https://www.youtube.com/embed/LMlbRsiYgpw",
+    }
+  ]
+  constructor(overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal) {
+    let parent = this;
+    this.filters.forEach(function(col, key) {
+      parent.filterDetailsArray[col.id] = col;
+    });
+  }
   onClick1() {
    if (this.isClicked) {
     this.isClicked = false;
