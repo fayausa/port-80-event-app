0377cdeb4f5d18b7b2385ccaa1d6f9b07cf018f9
Rejah Rehim <rejah@fayausa.com>
2017-07-04 13:04:56 +0530

    Updated the Data Json

--- src/app/home/schedule/schedule.component.ts
+++ src/app/home/schedule/schedule.component.ts
@@ -49,25 +49,87 @@ export class ScheduleComponent {
     },
     {
       "id": 7,
-      "title": "Tensor Flow",
+      "title": "PWA",
       "bg_color_class": "bg_yellow"
     },
     {
       "id": 8,
-      "title": "UX",
+      "title": "VR & AR",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 9,
+      "title": "IOT",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 10,
+      "title": "Bionics",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 11,
+      "title": "Workshop",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 12,
+      "title": "Civic Technologies",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 13,
+      "title": "Privacy",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 14,
+      "title": "NLP",
+      "bg_color_class": "bg_primary"
+    },
+    {
+      "id": 15,
+      "title": "Bioinformatics",
       "bg_color_class": "bg_primary"
     }
+
   ]
   schedule = [
     {
       "id": 1,
+      "title": "Registration",
+      "stage": "Registration Desk (Park Center)",
+      "time": "9:00 AM",
+    },
+    {
+      "id": 2,
+      "title": "Inauguration",
+      "stage": "Travancore Hall (Park Center)",
+      "time": "9:30 AM",
+    },
+    {
+      "id": 3,
       "title": "Tech Disruption – Opportunities & Threats",
       "speaker": "Mr. Michael Gord (Founder & CEO - MLG Blockchain Consulting)",
       "time": "10:00 AM",
       "stage": "Travancore Hall (Park Center)",
       "filters": [1,2],
       "video_url": "https://www.youtube.com/embed/LMlbRsiYgpw",
-    }
+    },
+    {
+      "id": 4,
+      "title": "Tea Break",
+      "time": "11:00 AM",
+    },
+    {
+      "id": 5,
+      "title": "FOSS - Role of communities",
+      "speaker": "Mr. Satish Babu (Chair, APRALO - ICANN)",
+      "time": "11:10 AM",
+      "stage": "Travancore Hall (Park Center)",
+      "filters": [3],
+      "video_url": "https://www.youtube.com/embed/Vtiqj1hCH4I",
+    },
   ]
   constructor(overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal) {
     let parent = this;
